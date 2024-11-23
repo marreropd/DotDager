a8cb38521c063d915aa18c1cd4fa550f0f08cf87
pablomarrero <pablomarrerofernandez1@gmail.com>
2024-11-23 05:16:55 +0100

    update

--- src/components/SoundCloudPlayer.tsx
+++ src/components/SoundCloudPlayer.tsx
@@ -19,12 +19,11 @@ const SoundCloudPlayer = () => {
             });
           }, 1300);
         }}
-        // style="position: absolute; top: -9999em; visibility: hidden;"
         width="100%"
         height="300"
         scrolling="no"
         allow="autoplay"
-        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1660528670&color=%23ffd399&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1660528670&color=%23ffd399&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
       ></iframe>
     </>
   );
